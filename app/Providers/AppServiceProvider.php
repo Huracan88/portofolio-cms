691417ae207c3663ff91f03e92eaf0cb28496295
Andres Pinto <aapcx55@gmail.com>
2026-09-19 14:52:24 -0500

    fix: force HTTPS in production and trust all proxies for Vite assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         app('filament-shield')->enforcePolicies();
 
         Blade::component('components.icon', 'icon');
